6ed7e626c988d80f466624e2a8b9f3d757ad362f
Moy_Gospodin <kamilxa200804@gmail.com>
2024-03-26 16:26:05 +0300

    forth try

--- sundaram.cpp
+++ sundaram.cpp
@@ -4,7 +4,7 @@
 std::vector<int> sieve(int n) {
     std::vector<int> primes;
     int k;
-    std::vector<int>mass(2*n+10,0);
+    std::vector<int>mass(2*n+2,0);
     mass[0]=1;
     for(int i=1;i<=(sqrt(2*n+1)-1)/2;i++)
     {
@@ -22,7 +22,7 @@ std::vector<int> sieve(int n) {
     }
     for(int i=0;i<n;i++)
     {
-        if(mass[i]==0&&i<=n)
+        if(mass[i]==0&&i*2+1<=n)
         {
             primes.push_back(i*2+1);
         }
