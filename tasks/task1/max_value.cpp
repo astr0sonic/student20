4457e41f83d19f080bbf1fa8ca3779bfcc376f0d
Moy_Gospodin <kamilxa200804@gmail.com>
2024-05-21 11:16:14 +0300

    sixth1

--- tasks/task1/max_value.cpp
+++
@@ -1,5 +0,0 @@
-#include "max_value.h"
-
-double calculateMaxValue(double* nums, int n) {
-    return 0.0;
-}
--- tasks/task2/eratosthenes.cpp
+++
@@ -1,7 +0,0 @@
-#include "eratosthenes.h"
-
-std::vector<int> sieve(int n) {
-    std::vector<int> primes;
-    primes.push_back(2);
-    return primes;
-}
